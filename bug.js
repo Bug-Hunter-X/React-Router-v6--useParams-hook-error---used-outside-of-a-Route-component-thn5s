6b9960d6fb0e4b23a65c60ecb6406db3b48e78cb
BugHunter-X <askzenai@gmail.com>
2024-12-22 17:40:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+In React Router Dom v6, a common error occurs when using the `useParams` hook within a component that's not directly nested within a route defined using the `<Route>` component.  This often manifests as `useParams() hook was used outside of a Route component` error message.
